93172c57de1c0cfad478f778846344f54c5f899e
Elvin Palma <enpalma@unah.hn>
2021-10-22 19:59:47 -0600

    Cambios

--- Tienda/Win.Tienda/FormModelo.cs
+++ Tienda/Win.Tienda/FormModelo.cs
@@ -23,6 +23,78 @@ namespace Win.Tienda
             listaModelosBindingSource.DataSource = _modelos.ObtenerModelos();
         }
 
-       
+        private void listaModelosBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaModelosBindingSource.EndEdit();
+            var modelo = (Modelo)listaModelosBindingSource.Current;
+
+            var resultado = _modelos.GuardarModelo(modelo);
+
+            if (resultado.Exitoso == true)
+            {
+                listaModelosBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+            }
+            else
+                {
+                MessageBox.Show(resultado.Mensaje);
+                }
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _modelos.AgregarModelo();
+            listaModelosBindingSource.MoveLast();
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            
+
+            if (idTextBox1.Text != "")
+            {
+                var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+                if (resultado == DialogResult.Yes)
+                {
+                    var id = Convert.ToInt32(idTextBox1.Text);
+                    Eliminar(id);
+                }
+                
+            }
+        }
+
+        private void Eliminar(int id)
+        {
+            var resultado = _modelos.EliminarModelo(id);
+
+            if (resultado == true)
+            {
+                listaModelosBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar el modelo");
+            }
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+            DeshabilitarHabilitarBotones(true);
+            Eliminar(0);
+        }
     }
 }
